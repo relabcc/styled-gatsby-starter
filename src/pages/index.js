bfaf97047a0e0d4faf3ed111fff4770a6e331651
Renddi Yeh <renddi@relab.cc>
2018-07-30 16:57:55 +0800

    BackgroudImge can control by height

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,7 +9,7 @@ import Button from '../components/Button';
 
 const Index = () => (
   <Container height="100%">
-    <Box py="2em">
+    <Box mx={[0, null, null, '3em']} py={[0, null, null, '2em']}>
       <Text.h1>Styled Gatsby Starter</Text.h1>
       <Text>Styled Components + Styled System is cool!</Text>
     </Box>
